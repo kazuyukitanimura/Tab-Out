b23150564f713cc44dc9c321369831246f95c447
Atsuya Takagi <atsuya.takagi@gmail.com>
2011-08-27 14:20:01 -0700

    aa;

--- app.js
+++ app.js
@@ -35,7 +35,6 @@ UserSchema.plugin(mongooseAuth, {
       myHostname: 'http://69.164.214.192'
       , consumerKey: config.auth.twitter.consumerKey
       , consumerSecret: config.auth.twitter.consumerSecret
-      , callbackPath : '/auth/twitter/callback/path'
       , redirectPath: '/'
     }
   }
