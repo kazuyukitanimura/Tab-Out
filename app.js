9fe78f709dd43eedd29f043e0074123c72ce97c6
Atsuya Takagi <atsuya.takagi@gmail.com>
2011-08-27 19:41:44 -0700

    adding index.

--- app.js
+++ app.js
@@ -83,7 +83,7 @@ app.configure('production', function(){
 app.get('/', function(req, res){
   console.log(req.loggedIn);
   if (req.loggedIn) {
-    res.send('hello ' + req.user.twit.name);
+    res.render('index');
   } else {
     res.redirect('/login');
   }
