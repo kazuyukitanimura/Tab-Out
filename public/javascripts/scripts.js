44d9321bd893b1040722a01d10b6841ec50048af
Tomomi Imura <tomomi.i@gmail.com>
2011-08-28 11:34:22 -0700

    populated form fields -creating a group name, and member name

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,9 +1,54 @@
  $(document).ready(function() {
+	
+	/* "About" Modal */
 	$("#about").click(function(e) {
-		
-	    /* "About" Modal */
 		e.preventDefault();
 		
+		openDialog();
+		
+		$('.close-dialog-button').click(function (e) {
+			e.preventDefault();
+			$('#scrim, #dialog').hide();
+		});
+	});
+	
+	/* Create a group */
+	$("#createGroupButton").click(function (e) {	
+	//$("#groupname").keyup(function () {	
+		var groupname = $("#groupname").val();
+		console.log(groupname);
+		
+        $.ajax({
+            url: "/groups/create",
+            data: "name=" + groupname,
+            //type: "post",
+            success: function () {
+				console.log("Success!");
+               	addMember();
+            }
+        });		
+
+	});
+	
+	function addMember () {
+		this.timer = setTimeout(function () {
+			$("#addNumber").css("display","block");
+		 }, 300);
+		
+		$("#numPeople").change(function () {	
+				var num = $("#numPeople").val();
+				console.log(num);
+				this.timer2 = setTimeout(function () {
+				$('#memberInputs').empty();
+				for (var i=0; i<num; i++) {
+					$('#memberInputs').append('<div class="number-input"><input type="text" id="user_id"' +i+ ' placeholder="Twitter ID or Email" /><input type="submit" id="addMemberButton" value="Add"></div>');
+				}
+				$("#addPeople").css("display","block");
+			});
+		});
+	}
+	
+	function openDialog () {
 		// Scrim
 		var scrimHeight = $(document).height();
 		var scrimWidth = $(window).width();
@@ -13,15 +58,9 @@
 		var winHeight = $(window).height();
        var winWidth = $(window).width();
 		
-		$("#dialog").css('top',  winHeight/2 - $("#dialog").height()/2);
-		$("#dialog").css('left', winWidth/2 - $("#dialog").width()/2);
+		$("#dialog").css("top",  winHeight/2 - $("#dialog").height()/2);
+		$("#dialog").css("left", winWidth/2 - $("#dialog").width()/2);
 
 		$("#dialog").fadeIn(800);
-		
-
-		$('.close-dialog-button').click(function (e) {
-			e.preventDefault();
-			$('#scrim, #dialog').hide();
-		});
-	});
+	}
 });
